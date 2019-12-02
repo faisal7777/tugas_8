cf6c09d504100dc481a6e5a575e488c31117cfa3
faisal7777 <faisal80050@gmail.com>
2019-12-02 00:49:20 -0800

    Tugas_8

---
+++ db/dbweb8.sql
@@ -0,0 +1,88 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 30, 2019 at 10:13 AM
+-- Server version: 10.1.22-MariaDB
+-- PHP Version: 7.1.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbweb8`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drzchat`
+--
+
+CREATE TABLE `drzchat` (
+  `nomor` int(3) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `pesan` varchar(200) NOT NULL,
+  `waktu` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `drzchat`
+--
+
+INSERT INTO `drzchat` (`nomor`, `nama`, `pesan`, `waktu`) VALUES
+(135, 'ani', 'sedang apa?', '16:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE `message` (
+  `message_id` int(11) NOT NULL,
+  `chat_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `username` char(50) DEFAULT NULL,
+  `message` char(50) DEFAULT NULL,
+  `post_time` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `drzchat`
+--
+ALTER TABLE `drzchat`
+  ADD PRIMARY KEY (`nomor`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`message_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `drzchat`
+--
+ALTER TABLE `drzchat`
+  MODIFY `nomor` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=136;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
